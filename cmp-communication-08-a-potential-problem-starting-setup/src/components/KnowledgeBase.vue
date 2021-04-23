c6bda7acb8bba031a9c22d2164cb9bf297c8dcba
Saulo <duart.herrera@gmail.com>
2021-04-23 16:10:43 -0300

    provide and inject methods to avoid pass trhough components, use with caution

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeBase.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <h2>Select a Topic</h2>
+    <!-- IF YOU HAVE A PASSING THROUGH COMPONENT USE PROVIDE AND INJECT, OTHERWISE GO WITH THE DEFAULT, PROPS AND EMMIT -->
+    <knowledge-grid></knowledge-grid>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
